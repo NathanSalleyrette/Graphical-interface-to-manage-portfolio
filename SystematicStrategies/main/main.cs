95ded0e5f9bc253a6c7476970be04443e1e77d5f
peyrichn <nicolas.peyrichou@grenoble-inp.org>
2021-09-02 14:50:52 +0200

    ajout d'un projet console
    Test sur les shareValue

--- SystematicStrategies/main/main.cs
+++ SystematicStrategies/main/main.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using PricingLibrary;
+using PricingLibrary.FinancialProducts;
+using PricingLibrary.Utilities.MarketDataFeed;
+
 namespace SystematicStrategies.main
 {
     class main
     {
-        PricingLibrary.FinancialProducts.VanillaCall n = new PricingLibrary.FinancialProducts.VanillaCall("oui", new PricingLibrary.FinancialProducts.Share("loic", "1"), new DateTime(), 23.0 );
+
+        public main()
+        {
+            VanillaCall n = new VanillaCall("oui", new Share("loic", "1"), new DateTime(), 23.0);
+            Share action = new Share("AIREBUS", "0");
+            ShareValue[] cours = new ShareValue[30];
+            for (int i = 0; i < 30; i++)
+            {
+                cours[i] = new ShareValue("0", new DateTime(2000, 01, i + 1), i);
+            }
+            Console.WriteLine(cours.ToString());
+
+        }
     }
 }
