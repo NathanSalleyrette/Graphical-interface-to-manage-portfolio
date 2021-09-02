c9b54dcbea9078d51684ea336d72b9162ceadcbd
Matthieu-Rambaud <matrambaud@yahoo.fr>
2021-09-03 00:16:26 +0200

    ajout d'un diagramme de classes de PricingLibrary + quelques ajouts pour demain

--- Console/Program.cs
+++ Console/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using PricingLibrary.FinancialProducts;
 using PricingLibrary.Utilities.MarketDataFeed;
+using PricingLibrary.Computations;
+using PricingLibrary.Utilities;
 
 namespace SystematicStrategies.main
 {
@@ -13,15 +15,38 @@ namespace SystematicStrategies.main
 
         public static void Main()
         {
-            VanillaCall n = new VanillaCall("oui", new Share("loic", "1"), new DateTime(), 23.0);
             Share action = new Share("AIREBUS", "0");
-            ShareValue[] cours = new ShareValue[30];
-            for (int i = 0; i < 30; i++)
+            SimulatedDataFeedProvider sdf = new SimulatedDataFeedProvider();
+            string[] ids = new string[] { "0" };
+            List<DataFeed> dfList = sdf.GetDataFeed(ids , new DateTime(2010, 01, 05), new DateTime(2010, 10, 30));
+            
+            foreach(DataFeed df in dfList)
             {
-                cours[i] = new ShareValue("0", new DateTime(2000, 01, i + 1), i);
-                Console.Write(cours[i].DateOfPrice);
+                Console.Write(df.Date);
+                Console.Write("\t");
+                Console.Write(string.Join(Environment.NewLine, df.PriceList));
+                Console.Write("\n");
             }
-            VanillaCall opt = new VanillaCall("Call", action, new DateTime(2000, 01, 30), 25); 
+
+            VanillaCall opt = new VanillaCall("VCall", action, new DateTime(2010, 10, 30), 10.5);
+            var Pricer = new Pricer();
+
+            var V0 = 100;
+            var T0 = new DateTime(2010, 01, 05);
+            var cours0 = decimal.ToDouble(dfList[0].PriceList["0"]);  // égale à 10
+            PricingResults result0 = Pricer.Price(opt, T0, 360, cours0, 0.25);
+
+            var S0 = result0.Price;
+            var delta0 = result0.Deltas[0];
+
+            Console.Write(S0);
+            Console.Write("\n");
+            Console.Write(delta0);
+            Console.Write("\n");
+
+
+
+
 
         }
     }
