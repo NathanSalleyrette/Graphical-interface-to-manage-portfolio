ac812c8cb72fea8c8100e868d261b1268b2da595
simeonl <loic.simeon@grenoble-inp.org>
2021-09-02 15:11:37 +0200

    uniformstrategie

---
+++ SystematicStrategies/Strategies/Strategie.cs
@@ -0,0 +1,18 @@
+﻿using PricingLibrary.Utilities.MarketDataFeed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystematicStrategies.Strategies
+{
+    abstract class Strategie
+    {
+
+
+        abstract public bool Rebalencing(DateTime t);
+
+        abstract public Dictionary<string, double> UpdateCompo(DateTime t, double value, DataFeed market);
+    }
+}
