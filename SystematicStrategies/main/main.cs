d23dade8938aeee7b312800066914947d088b932
simeonl <loic.simeon@grenoble-inp.org>
2021-09-02 11:35:37 +0200

    ajout de pricingLibrary
    J'ai tout fé

---
+++ SystematicStrategies/main/main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PricingLibrary;
+namespace SystematicStrategies.main
+{
+    class main
+    {
+        PricingLibrary.FinancialProducts.VanillaCall n = new PricingLibrary.FinancialProducts.VanillaCall("oui", new PricingLibrary.FinancialProducts.Share("loic", "1"), new DateTime(), 23.0 );
+    }
+}
