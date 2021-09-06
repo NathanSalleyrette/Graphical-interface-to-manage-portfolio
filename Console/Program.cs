5525448fcc0f65d3233848e613d3aeb775a8ff27
simeonl <loic.simeon@grenoble-inp.org>
2021-09-06 16:23:00 +0200

    changes

--- Console/Program.cs
+++ Console/Program.cs
@@ -4,34 +4,81 @@ using System.Linq;
 using PricingLibrary.FinancialProducts;
 using PricingLibrary.Utilities.MarketDataFeed;
 using PricingLibrary.Utilities;
+using System.Runtime.InteropServices;
 
 namespace SystematicStrategies.main
 {
     class Program
     {
+        // import WRE dlls
+        [DllImport(@"wre-ensimag-c.dll", EntryPoint = "WREmodelingCov", CallingConvention = CallingConvention.Cdecl)]
 
-        public static void Main()
+        // declaration
+        public static extern int WREmodelingCov(
+            ref int returnsSize,
+            ref int nbSec,
+            double[,] secReturns,
+            double[,] covMatrix,
+            ref int info
+        );
+
+        [DllImport(@"wre-ensimag-c.dll", EntryPoint = "WREanalysisExpostVolatility", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int WREanalysisExpostVolatility(ref int nbValues, double[,] portfolioreturns, double[,] expostVolatility, ref int info);
+        public static double[,] computeCovarianceMatrix(double[,] returns)
         {
-            Share action = new Share("AC FP", "AC FP");
-            SemiHistoricDataFeedProvider sdf = new SemiHistoricDataFeedProvider();
-            double nbDaysPerYear = sdf.NumberOfDaysPerYear;
-            string[] ids = new string[] { "AC FP" };
-            List<DataFeed> dfList = sdf.GetDataFeed(ids , new DateTime(2010, 02, 05), new DateTime(2012, 12, 30));
+            int dataSize = returns.GetLength(0);
+            int nbAssets = returns.GetLength(1);
+            double[,] covMatrix = new double[nbAssets, nbAssets];
+            int info = 0;
+            int res;
+            res = WREmodelingCov(ref dataSize, ref nbAssets, returns, covMatrix, ref info);
+            if (res != 0)
+            {
+                if (res < 0)
+                    throw new Exception("ERROR: WREmodelingCov encountred a problem. See info parameter for more details");
+                else
+                    throw new Exception("WARNING: WREmodelingCov encountred a problem. See info parameter for more details");
+            }
+            return covMatrix;
+        }
 
-            var firstMarket = dfList[0];
-            dfList.RemoveAt(0);
-            List<double> volatilities = new List<double>();
-            double mean = 0;
+        public static void dispMatrix(double[,] myCovMatrix)
+        {
+            int n = myCovMatrix.GetLength(0);
+
+            Console.WriteLine("Covariance matrix:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(myCovMatrix[i, j] + "\t");
+                }
+                Console.Write("\n");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // header
+            Console.WriteLine("******************************");
+            Console.WriteLine("*    WREmodelingCov in C#   *");
+            Console.WriteLine("******************************");
+
+            // sample data
+            SemiHistoricDataFeedProvider sdf = new SemiHistoricDataFeedProvider();
+            string[] ids = new string[] { "AC FP" };
+            List<DataFeed> dfList = sdf.GetDataFeed(ids, new DateTime(2010, 01, 05), new DateTime(2010,10,30));
+            List<double> portfolioreturns = new List<double>(); 
+
             foreach(var df in dfList)
             {
-                volatilities.Add( Math.Abs( Math.Log((double)(df.PriceList["AC FP"]) / (double)(firstMarket.PriceList["AC FP"])) )/ Math.Sqrt(1/ nbDaysPerYear ));
-                
-                firstMarket = df;
-                mean += volatilities.Last();
+                portfolioreturns.Add((double)df.PriceList["AC FP"]);
             }
 
-            Console.WriteLine(mean / dfList.Count);
-            Console.ReadLine();
+            double[,] returns;
+            var nbValues = DayCount.CountBusinessDays(new DateTime(2010, 01, 05), new DateTime(2010, 10, 30));
+            int info = 0;
+            int vol = WREanalysisExpostVolatility(ref nbValues, portfolioreturns.ToArray(), returns, ref info);
         }
     }
 }
