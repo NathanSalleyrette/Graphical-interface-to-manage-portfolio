e2e43bd08f18b1904b3d20d10ab9f0a6dde9a627
simeonl <loic.simeon@grenoble-inp.org>
2021-09-07 11:03:13 +0200

    test for volatility and covmatrix

--- Console/Program.cs
+++ Console/Program.cs
@@ -65,27 +65,63 @@ namespace SystematicStrategies.main
             Console.WriteLine("******************************");
 
             // sample data
-            SemiHistoricDataFeedProvider sdf = new SemiHistoricDataFeedProvider();
-            string[] ids = new string[] { "AC FP" };
-            List<DataFeed> dfList = sdf.GetDataFeed(ids, new DateTime(2010, 01, 05), new DateTime(2012,12,30));
+            SimulatedDataFeedProvider sdf = new SimulatedDataFeedProvider();
+            string[] ids = new string[] { "AC FP", "ACA FP" };
+            List<DataFeed> dfList = sdf.GetDataFeed(ids, new DateTime(2000, 01, 05), new DateTime(2021,12,30));
             int nbValues = dfList.Count;
             double[] portfolioreturns = new double[nbValues];
 
             for (int i = 1; i < nbValues; i++)
             {
-                portfolioreturns[i] = Math.Log((double)dfList[i].PriceList["AC FP"] / (double)dfList[i-1].PriceList["AC FP"])* Math.Sqrt(252) ;
+                double log = Math.Log((double)dfList[i].PriceList["AC FP"] / (double)dfList[i - 1].PriceList["AC FP"]);
+                portfolioreturns[i] = log * Math.Sqrt(sdf.NumberOfDaysPerYear/ DayCount.CountBusinessDays(dfList[i-1].Date, dfList[i].Date) );
             }
 
-            //int nbValues = 214;
-
             int info = 0;
-
             double[] vol = { 0 };
             nbValues--;
-            int v = WREanalysisExpostVolatility(ref nbValues , portfolioreturns, vol , ref info);
-            Console.WriteLine(v);
+            int res = WREanalysisExpostVolatility(ref nbValues , portfolioreturns, vol , ref info);
+            if (res != 0)
+            {
+                if (res < 0)
+                    throw new Exception("ERROR: WREanalysisExpostVolatility encountred a problem. See info parameter for more details");
+                else
+                    throw new Exception("WARNING: WREanalysisExpostVolatility encountred a problem. See info parameter for more details");
+            }
             Console.WriteLine(vol[0] );
-            Console.ReadLine(); 
+
+            double[,] covportfolioreturns = new double[dfList.Count, ids.Length];
+
+            int dataSize = covportfolioreturns.GetLength(0);
+            int nbAssets = covportfolioreturns.GetLength(1);
+
+            for (int i = 1; i < dataSize; i++)
+            {
+                for (int j = 0; j < nbAssets; j++)
+                {
+                    double log = (double)dfList[i].PriceList[ids[j]] - (double)dfList[i - 1].PriceList[ids[j]];
+                    covportfolioreturns[i, j] = log;
+                }
+            }
+
+            double[,] covMatrix = new double[nbAssets, nbAssets];
+
+            info = 0;
+            res = WREmodelingCov(ref dataSize, ref nbAssets, covportfolioreturns, covMatrix, ref info);
+            if (res != 0)
+            {
+                if (res < 0)
+                    throw new Exception("ERROR: WREmodelingCov encountred a problem. See info parameter for more details");
+                else
+                    throw new Exception("WARNING: WREmodelingCov encountred a problem. See info parameter for more details");
+            }
+
+            dispMatrix(covMatrix);
+
+            dispMatrix(computeCovarianceMatrix(covportfolioreturns));
+
+            Console.ReadLine();
+
         }
     }
 }
